7c9852df3e54cf71d8ea07e471ce99bf17daf05a
ShubhangiNage17 <nage.shubhangi97@gmail.com>
2024-09-04 02:13:25 +0530

    hyper-parameter tunning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,24 +12,20 @@ from src.components.data_transformation import DataTransformationConfig
 
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
-
-
-
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifact',"train.csv")
-    test_data_path: str=os.path.join('artifact',"test.csv")
-    raw_data_path: str=os.path.join('artifact',"data.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
 
-
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv(r'F:\ML_projects\End_To_End_Ml_Project\src\notebook\Data\StudentsPerformance.csv')
+            df = pd.read_csv(r'F:\ML_projects\End_To_End_Ml_Project\src\notebook\Data\StudentsPerformance.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -43,18 +39,16 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("ingestion of the data is completed")
-         
+            logging.info("Inmgestion of the data iss completed")
+
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-         
-            )
 
+            )
         except Exception as e:
             raise CustomException(e,sys)
-      
-
+        
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
@@ -62,6 +56,5 @@ if __name__=="__main__":
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-    modeltrainer = ModelTrainer()
+    modeltrainer=ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
-
